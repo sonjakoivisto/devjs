a06cf412038d56cf9903a2f39fbf8563a3885f57
sonjakoivisto <37457729+sonjakoivisto@users.noreply.github.com>
2018-08-14 21:14:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var firstName = prompt("what's your first name?");
+var lastName = prompt("what's your last name?");
+var age = prompt("what's your age?");
+console.log("Nice to meet you " + firstName + " " + lastName);
+console.log("You are " + age + " years old");
+
+var ikä = prompt("how old are you?");
+var days = ikä * 365.25;
+alert("You have been alive for " + days + " days");
