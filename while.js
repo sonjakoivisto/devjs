35ee783d26ae5cb44b959fb751c92e297595d06b
sonjakoivisto <37457729+sonjakoivisto@users.noreply.github.com>
2018-08-15 17:22:42 +0300

    Add files via upload

---
+++ while.js
@@ -0,0 +1,22 @@
+alert(hello);
+var num = -10;
+while(num < 20){
+    console.log(num)
+    num++;
+}
+var number = 10;
+while(number < 41){
+    console.log(number);
+    number+=2;
+}
+var num = 301;
+while(num <= 333){
+    console.log(num);
+    num+=2;
+}
+var num = 5;
+while(num < 50){
+    if(num % 5 === 0 && num % 3 === 0)
+        console.log(num);
+    num++;    
+}
