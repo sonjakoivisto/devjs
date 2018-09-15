f063ed412c2e962152013539751ba5c60e573515
sonjakoivisto <37457729+sonjakoivisto@users.noreply.github.com>
2018-09-15 19:41:11 +0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,11 @@
+var colors = ["light blue", "blue", "orange"];
+colors.push("yellow")
+colors.pop()
+colors.unshift("red")
+
+var nums = [34, 54, 22];
+nums.unshift("Hello");
+nums.unshift("pöö");
+nums.unshift("hei");
+// indexOf, Slice
+
