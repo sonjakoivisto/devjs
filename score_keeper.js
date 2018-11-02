ba4aa76912b60ac0bac4c793cd8e3fdc119310f8
sonjakoivisto <37457729+sonjakoivisto@users.noreply.github.com>
2018-11-02 18:29:53 +0200

    Add files via upload

--- score_keeper.js
+++ score_keeper.js
@@ -1,5 +1,8 @@
 var p1Button = document.querySelector("#p1");
-var p2Button = document.querySelector("#p2"); 
+var p2Button = document.querySelector("#p2");
+var resetButton = document.getElementById("reset"); 
+var numInput = document.querySelector("input");
+var winningScoreDisplay = document.querySelector("p span");
 var p1Score = 0;
 var p2Score = 0;
 var p1Display = document.querySelector("#p1Display");
@@ -7,10 +10,12 @@ var p2Display = document.querySelector("#p2Display");
 var gameOver = false;
 var winningScore = 5; 
 
+
 p1Button.addEventListener("click", function(){
     if(!gameOver){
         p1Score++;
         if(p1Score === winningScore){
+            p1Display.classList.add("winner"); 
             gameOver = true;
         } p1Display.textContent = p1Score;
     } 
@@ -21,6 +26,27 @@ p2Button.addEventListener("click", function(){
         p2Score++;
         if(p2Score === winningScore){
             gameOver = true;
+            p2Display.classList.add("winner"); 
         } p2Display.textContent = p2Score;
     }
 });
+
+resetButton.addEventListener("click", function(){
+    reset();
+}); 
+
+numInput.addEventListener("change", function(){
+    winningScoreDisplay.textContent = this.value; 
+    winningScore = String(this.value);
+    reset();
+});
+ 
+function reset(){
+        p1Score = 0;
+        p2Score = 0;
+        p1Display.textContent = 0;
+        p2Display.textContent = 0;
+        p1Display.classList.remove("winner");
+        p2Display.classList.remove("winner");
+        gameOver = false;
+};
